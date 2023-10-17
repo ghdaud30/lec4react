a06cf91889c72edb59699d3f147767f750e292ef
ghdaud30 <54072339+ghdaud30@users.noreply.github.com>
2023-10-17 11:40:22 +0900

    부산축제데이터

--- src/09/Busan.js
+++ src/09/Busan.js
@@ -1,67 +1,33 @@
-import React from 'react'
-import pusandata from './pusandata.json'
-import ButtonBlue from '../comm/ButtonBlue';
-import { useEffect, useRef, useState } from 'react';
-
+import pusandata from "./pusandata.json";
+// import ButtonBlue from "../comm/ButtonBlue";
+import GalleryCard from "../comm/GalleryCard";
+// import { useEffect, useRef, useState } from "react";
 const Busan = () => {
-
-    //select box
-    const sel = useRef();
-
-    console.log(pusandata);
-
-    let code = [];
-
-    code = pusandata.map((item, idx) =>
-        [item.콘텐츠ID, item.콘텐츠명.slice(0, item.콘텐츠명.lastIndexOf('('))]
-    );
-    console.log(code)
-    const opTag = code.map((item) =>
-        <option key={item[0]} value={item[1]}>{item[1]}</option>
-    );
-
-
-    const handleOk = (e,msg) => {
-        e.preventDefault();
-        console.log(msg);
-    }
-
-    const handleCancel = (e, msg) => {
-        e.preventDefault();
-        console.log(msg);
-    }
-
-    const handleChange = () => {
-        console.log(sel.current.value);
-    }
-
-    useEffect(() => {
-        sel.current.focus();
-    },[]);
+    const items = pusandata.map((item) => 
+    // console.log(item)
+        <GalleryCard 
+        key = {item.콘텐츠ID}
+        imgsrc={item.썸네일이미지URL} 
+        title={item.제목} 
+        content={item.상세내용.length < 100 ? item.상세내용 : item.상세내용.substring(0, 100) + '...'}  
+        // content={item.상세내용.substring(0, 100) + '...'}  
+        // content={item.ITEMCNTNTS}  
+        sptag={item.주요장소.indexOf < 0 ?[item.주요장소] :item.주요장소.split(',')}  />
+    ) ;
 
     return (
-        <main className='container'>
-            <article className="flex justify-between items-center">
-                <h2 className='text-2xl font-bold'>부산축제정보</h2>
-            </article>
-            <div className='grid grid-cols-4 gap-4'>
-                <div className='col-span-2'>
-                    <select ref = {sel} id='sel' name='sel' onChange={() => handleChange()}>
-                        <option value =''>축제명을 선택하세요</option>
-                        {opTag}
-                    </select>
+        <main className="container">
+            <article>
+                <header>
+                    <h1 className="text-2xl font-bold">부산축제정보</h1>
+                </header> 
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {items}
                 </div>
-                <div>
-                    <ButtonBlue caption= '축제확인' handleClick={(e) => handleOk(e,'ok')}></ButtonBlue>
-                </div>
-                <div>
-                    <ButtonBlue caption= '축제취소' handleClick={(e) => handleCancel(e,'cancel')}></ButtonBlue>
-                </div>
-                
-            </div>
+            </article> 
+            
         </main>
     )
-
 }
 
 export default Busan
